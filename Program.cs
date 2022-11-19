a5f7b27e18c2191f81b24333b42a0650336ddbd4
Tom <intrinsicdev@outlook.com>
2022-11-18 21:51:25 -0500

    Working copy

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using xTF2Console;
+
+var exitSignal = ".exit";
+
+var rcon = new RemoteConsole();
+var titleID = rcon.GetTitleID();
+
+Console.WriteLine("Waiting for The Orange Box to start");
+while (titleID != 0x4541080f)
+{
+	Thread.Sleep(5000);
+	titleID = rcon.GetTitleID();
+}
+
+Console.WriteLine($"The Orange Box Detected. Begin sending commands once the game is fully loaded (background map is playing.)");
+Console.WriteLine($"Type {exitSignal} to exit");
+
+string? cmd = Console.ReadLine();
+
+while (cmd != exitSignal) 
+{
+	if (cmd == null) break;
+	rcon.SendCommand(cmd);
+	cmd = Console.ReadLine();
+}
---
+++ RemoteConsole.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Test.Xbox.XDRPC;
+using XDevkit;
+using XRPCLib;
+
+namespace xTF2Console
+{
+	internal class RemoteConsole
+	{
+		private XRPC xrpc;
+		internal RemoteConsole()
+		{
+			xrpc = new XRPC();
+			xrpc.Connect();
+
+			xrpc.Notify(XRPC.XNotiyLogo.UPDATING, "xTF2Console Connected");
+		}
+
+		internal void SendCommand(string command)
+		{
+			// tob
+
+			// cbuff_addtext
+			xrpc.Call(0x8609A848, new object[] { command });
+			// cbuff_execute
+			xrpc.Call(0x8609CE90, new object[] { null });
+		}
+
+		internal uint GetTitleID()
+		{
+			return xrpc.SystemCall(new object[] { xrpc.ResolveFunction("xam.xex", 463) });
+		}
+	}
+}
